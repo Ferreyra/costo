c8feb508d75bfb589e2c45cbd802211623b0ff84
Roberto Ferreyra <robfercor@gmail.com>
2022-06-01 18:13:27 -0500

    Modificacion operaciones matematicas

--- scripts.js
+++ scripts.js
@@ -4,36 +4,77 @@ const inputCosto = document.getElementById('cost')
 const inputEnvio = document.getElementById('envio')
 const inputGanacia = document.getElementById('ganancia')
 const tablaProductos = document.getElementById('tpp')
+const guia = document.getElementById('txtGuia')
 
 let numArticulos
+let articulosIngredados = 0
 let costoTotal = 0
 let envio = 0
 let ganancia = 0
 
-function calcPrecio() { 
-  console.log('precio ') 
-  let item = 'prod' + this.attributes['data-pzs'].value
+function calcPrecio(refProd) {   
+  if (refProd.target !== undefined)
+    refProd = this   
+  let item = 'prod' + refProd.attributes['data-pzs'].value
   let costoPiezas = document.getElementById(item).value
-  let costoUnitario = (costoPiezas/this.value)
-  
+// TODO: validar costoPiezas 
+  let costoUnitario = (costoPiezas/refProd.value)  
   let precioVenta = costoUnitario * (1 + (ganancia/100))
-  if (envio) {
-    precioVenta += (envio/numArticulos)/this.value
-  }
-  console.log(precioVenta)
-  let concatIdPrecio = 'precioU' + this.attributes['data-pzs'].value
-  let concatIdCosto = 'costU' + this.attributes['data-pzs'].value
-  const costoElemt = document.getElementById(concatIdCosto)
-  const precioElemt = document.getElementById(concatIdPrecio)
+  if (envio) 
+    precioVenta += (envio/numArticulos)/refProd.value
+  
+  let idPrecio = 'precioU' + refProd.attributes['data-pzs'].value
+  let idCosto = 'costU' + refProd.attributes['data-pzs'].value
+
+  const costoElemt = document.getElementById(idCosto)
+  const precioElemt = document.getElementById(idPrecio)
   costoElemt.innerHTML = `$ ${costoUnitario.toFixed(1)}`
-  precioElemt.innerHTML = `$ ${precioVenta.toFixed(1)}`  
+  precioElemt.innerHTML = `$ ${precioVenta.toFixed(1)}`
+
+
+  const precioElements = document.getElementsByClassName('cel precio')
+  let sumaPrecios = 0
+  for (const precioElement of precioElements) {
+    if (precioElement.textContent !== "$") {
+      let strP = precioElement.textContent.split(' ')
+      console.log(parseFloat(strP[1]))
+      sumaPrecios += parseFloat(strP[1]) * refProd.value
+    }
+  }
+  console.log('suma precios:', sumaPrecios)
+
+  const costoElements = document.getElementsByClassName('cost')
+  let sumaCostos = 0
+  for (const costoIngresado of costoElements) {
+    if (costoIngresado.value)
+      sumaCostos += parseFloat(costoIngresado.value)
+  }
+  console.log(sumaCostos)
+  console.log('ganancia:', sumaPrecios -  sumaCostos)
 }
+
+function logKey(e) {
+  if (e.keyCode === 13)
+    if (this.value)
+      calcPrecio(this)
+}
+
+inputArticulos.addEventListener('focus', () => {
+  const childConunt = productosParent.childElementCount
+  if (childConunt > 2) {
+    for (let index = childConunt-1; index > 1; --index) {
+      if (productosParent.children[index].attributes['class'].value == 'row')
+        productosParent.children[index].remove()
+    }    
+  }
+})
 
 inputArticulos.addEventListener('focusout', () => {
   numArticulos = inputArticulos.value
 
   if (numArticulos >= 1) {
     tablaProductos.removeAttribute('style')
+    guia.style.display = 'none'
     for (let articulo = 1; articulo <= numArticulos; articulo++) {
       productosParent.innerHTML += `
         <div class="row">
@@ -49,7 +90,7 @@ inputArticulos.addEventListener('focusout', () => {
             </label>
           </div>
           <div class="cel" id="costU${articulo}">$</div>
-          <div class="cel" id="precioU${articulo}">$</div>
+          <div class="cel precio" id="precioU${articulo}">$</div>
         </div>        
       `      
     }
@@ -64,8 +105,8 @@ inputArticulos.addEventListener('focusout', () => {
     `*/
     const inputPiezas = document.querySelectorAll('[data-pzs]')
     inputPiezas.forEach(input => {
-      console.log('addEvent')
       input.addEventListener('focusout', calcPrecio)
+      input.addEventListener('keyup', logKey)
     });
   }
 })
