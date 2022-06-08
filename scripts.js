e2e16fb8e9ad43e8fda42711bb3f63d5ad32a6ac
Roberto Ferreyra <robfercor@gmail.com>
2022-06-08 18:12:07 -0500

    Tabla responsive

--- scripts.js
+++ scripts.js
@@ -57,23 +57,22 @@ function actulizarLista(refProd) {
     refProd = this
 
   let strId = String(refProd.attributes['id'].value)
-  let item = parseInt(strId.substring(strId.length - 1)) - 1
-  const productoSel = listaPrecios.producto[item]
-  console.log(productoSel)
+  let item = parseInt(strId.substring(strId.length - 1))
+  const productoSel = listaPrecios.producto[item - 1]
 
   let esCosto = strId.substring(0, 4) 
   if (esCosto === 'cost') {
     productoSel.costo = refProd.value
     if (productoSel.cantidad > 0) {
       productoSel.precio = productoSel.calcPrecio(listaPrecios.ganancia, listaPrecios.envio/listaPrecios.numPoductos)
-      cambPrecio(item+1, productoSel.costUnit, productoSel.precio)
+      cambPrecio(item, productoSel.costUnit, productoSel.precio)
     }
   }
   if (esCosto === 'cant') {
     productoSel.cantidad = refProd.value
     if (productoSel.costo > 0) {
       productoSel.precio = productoSel.calcPrecio(listaPrecios.ganancia, listaPrecios.envio/listaPrecios.numPoductos)
-      cambPrecio(item+1, productoSel.costUnit, productoSel.precio)
+      cambPrecio(item, productoSel.costUnit, productoSel.precio)
     }
   }  
 }
@@ -86,7 +85,7 @@ function logKey(e) {
 
 inputArticulos.addEventListener('focus', () => {
   const childConunt = productosParent.childElementCount
-  if (childConunt > 2) {
+  if (childConunt > 1) {
     tablaProductos.style.display = 'none'
     for (let index = childConunt-1; index > 1; --index) {
       if (productosParent.children[index].attributes['class'].value == 'row')
@@ -96,61 +95,92 @@ inputArticulos.addEventListener('focus', () => {
 })
 
 inputArticulos.addEventListener('focusout', () => {
-  numArticulos = inputArticulos.value
-  listaPrecios.numPoductos = inputArticulos.value
+  if (inputArticulos.value) {
+    listaPrecios.numPoductos = inputArticulos.value
 
-  for (let index = 0; index < listaPrecios.numPoductos; index++) {
-    listaPrecios.producto.push(new Producto('Producto ' + (index + 1)))    
-  }
-
-  if (listaPrecios.numPoductos >= 1) {
-    tablaProductos.removeAttribute('style')
-    guia.style.display = 'none'
-    for (let articulo = 1; articulo <= listaPrecios.numPoductos; articulo++) {
-      productosParent.innerHTML += `
-        <div class="row">
-          <div class="cel">Productos ${articulo}<small>Descripción</small></div>
-          <div class="cel">
-            <label for="cost${articulo}">
-              <input type="number" class="cost" id="cost${articulo}" name="cost${articulo}" min="1">
-            </label>
-          </div>
-          <div class="cel">
-            <label for="cant${articulo}">
-              <input type="number" class="cant" data-pzs=${articulo} id="cant${articulo}" name="cant${articulo}" min="1">
-            </label>
-          </div>
-          <div class="cel" id="costU${articulo}">$</div>
-          <div class="cel precio" id="precioU${articulo}">$</div>
-        </div>        
-      `      
+    for (let index = 0; index < listaPrecios.numPoductos; index++) {
+      listaPrecios.producto.push(new Producto('Producto ' + (index + 1)))    
     }
-    /*productosParent.innerHTML += `
-      <div class="row totales">
-        <div class="cel">Total</div>
-        <div class="cel">$ 00</div>
-        <div class="cel">00</div>
-        <div class="cel">$ ++</div>
-        <div class="cel">$ +++</div>
-      </div>
-    `*/
-    const inputsProduct = productosParent.querySelectorAll('input')    
-    inputsProduct.forEach(input => {
-      input.addEventListener('focusout', actulizarLista)
-      input.addEventListener('keyup', logKey)
-    });
+
+    if (listaPrecios.numPoductos >= 1) {
+      tablaProductos.removeAttribute('style')
+      guia.style.display = 'none'
+      for (let articulo = 1; articulo <= listaPrecios.numPoductos; articulo++) {
+        productosParent.innerHTML += `
+          <div class="row">
+            <div class="cel"><span>Producto</span><h4>Producto ${articulo} <small>Descripción</small></h4></div>
+            <div class="cel">
+              <span>Costo</span>
+              <label for="cost${articulo}">
+                <input type="number" class="cost" id="cost${articulo}" name="cost${articulo}" min="1">
+              </label>
+            </div>
+            <div class="cel">
+              <span>Cantidad</span>
+              <label for="cant${articulo}">
+                <input type="number" class="cant" data-pzs=${articulo} id="cant${articulo}" name="cant${articulo}" min="1">
+              </label>
+            </div>
+            <div class="cel" id="costU${articulo}"><span>Costo unitario</span>$</div>
+            <div class="cel precio" id="precioU${articulo}"><span>Precio venta</span>$</div>
+          </div>        
+        `      
+      }
+      /*productosParent.innerHTML += `
+        <div class="row totales">
+          <div class="cel">Total</div>
+          <div class="cel">$ 00</div>
+          <div class="cel">00</div>
+          <div class="cel">$ ++</div>
+          <div class="cel">$ +++</div>
+        </div>
+      `*/
+      const inputsProduct = productosParent.querySelectorAll('input')    
+      inputsProduct.forEach(input => {
+        input.addEventListener('focusout', actulizarLista)
+        input.addEventListener('keyup', logKey)
+      });
+    }
   }
 })
 
-inputCosto.addEventListener('focusout', () => {  
-  listaPrecios.costo = inputCosto.value
+inputCosto.addEventListener('focusout', () => {
+  if (inputCosto.value)
+    listaPrecios.costo = inputCosto.value
 })
 
 inputEnvio.addEventListener('focusout', () => {
-  listaPrecios.envio = inputEnvio.value
+  if (inputGanacia.value)
+    listaPrecios.envio = inputEnvio.value
+})
+
+inputEnvio.addEventListener('keyup', (key) => {
+  if (key.key === "Enter")
+    if (inputEnvio.value) {
+      listaPrecios.envio = inputEnvio.value
+      const listPrcios = productosParent.getElementsByClassName('cant')
+      if (listPrcios.length > 0) {
+        for(let producto of listPrcios) {
+          actulizarLista(producto)
+        }
+      }
+    }
 })
 
 inputGanacia.addEventListener('focusout', () => {
-  listaPrecios.ganancia = inputGanacia.value
+  if (inputGanacia.value)
+    listaPrecios.ganancia = inputGanacia.value
 })
 
+inputGanacia.addEventListener('keyup', (key) => {
+  if (key.key === "Enter")
+    if (inputGanacia.value) {
+      listaPrecios.ganancia = inputGanacia.value
+      const listPrcios = productosParent.getElementsByClassName('cant')
+      if (listPrcios.length > 0) {
+        for(let producto of listPrcios) {
+          actulizarLista(producto)
+        }
+      }
+    }
+})
